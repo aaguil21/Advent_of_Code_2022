9a9c5f4e587863106657621f09c86eb59fd1c955
aaguil21 <68845693+aaguil21@users.noreply.github.com>
2022-12-02 11:30:42 -0600

    Createa script for day 1

---
+++ day1_code.py
@@ -0,0 +1,18 @@
+#Day 1 of Advent of Code challenge 
+
+filename = "input_Day1.txt"
+
+cal = [l.strip() for  l in open(filename)]
+
+elves = ('\n').join(cal).split('\n\n')
+
+def splitsum (x):
+    y = list(map(int, x.split()))
+    return sum(y)
+
+sum_elves = list(map(splitsum, elves))
+sum_elves.sort(reverse=True)
+
+print(sum_elves[0])
+print(sum(sum_elves[0:3]))
+

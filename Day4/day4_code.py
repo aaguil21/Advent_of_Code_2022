fc5b1cdce2836b6780697735df8e9498c1d53104
aaguil21 <68845693+aaguil21@users.noreply.github.com>
2022-12-06 11:09:32 -0600

    Added day5

--- Day4/day4_code.py
+++ Day4/day4_code.py
@@ -1,10 +1,37 @@
 import os 
-import string
-from functools import reduce
-import numpy as np
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-filename = "input_day3.txt"
+filename = "input_day4.txt"
 
 x = [l.strip() for  l in open(filename)]
+
+def tasks(x):
+    elves = x.split(",")
+    for elf in elves:
+        elf = elf.split("-")
+        yield elf
+
+y = list(map(list, map(tasks, x)))
+
+#Part One
+def encapsule (x):
+    elf1 = list(range(int(x[0][0]), int(x[0][1])+1))
+    elf2 = list(range(int(x[1][0]), int(x[1][1])+1))
+    if (set(elf1).issubset(elf2)) or (set(elf2).issubset(elf1)):
+        return 1
+    else:
+        return 0
+
+print(sum(list(map(encapsule, y))))
+
+#Part Two
+def overlap (x):
+    elf1 = list(range(int(x[0][0]), int(x[0][1])+1))
+    elf2 = list(range(int(x[1][0]), int(x[1][1])+1))
+    for i in elf1:
+        if i in elf2:
+            return 1
+    return 0
+
+print(sum(list(map(overlap, y))))

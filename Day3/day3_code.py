2e1396af18f4cf8f5bc685f2a7f3eb157afeadd1
aaguil21 <68845693+aaguil21@users.noreply.github.com>
2022-12-05 20:37:19 -0600

    Added day4

--- Day3/day3_code.py
+++ Day3/day3_code.py
@@ -1,6 +1,7 @@
 import os 
 import string
 from functools import reduce
+import numpy as np
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -12,6 +13,7 @@ alphabet = list(string.ascii_letters)
 numbers = list(range(1,53))
 priority = dict(zip(alphabet, numbers))
 
+##Part One
 def splitstring(value):
     string1, string2 = value[:len(value)//2], value[len(value)//2:]
     for letter in string1:
@@ -20,6 +22,13 @@ def splitstring(value):
 
 print(sum(list(map(splitstring, x))))
 
+##Part Two
+y = np.reshape(x, (len(x)//3, 3))
 
+def group_elves(group):
+    for let in group[0]:
+        if (let in group[1]) and (let in group[2]):
+            return priority[let]
 
+print(sum(list(map(group_elves, y))))
 
---
+++ Day4/day4_code.py
@@ -0,0 +1,10 @@
+import os 
+import string
+from functools import reduce
+import numpy as np
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+filename = "input_day3.txt"
+
+x = [l.strip() for  l in open(filename)]

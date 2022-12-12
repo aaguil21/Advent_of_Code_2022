974a2076805ac2ca80dc60a82dd55966ccc5ecf2
aaguil21 <68845693+aaguil21@users.noreply.github.com>
2022-12-12 16:55:02 -0600

    fix

--- Day11/day11_code.py
+++ Day11/day11_code.py
@@ -8,20 +8,24 @@ def split_input(filename: str) -> list:
     x = ('\n').join(input).split('\n\n')
     y = ('\n').join(x).split('\n\n')
 
-    monkeys = 
+    monkeys = []
     for i, monkey in enumerate(y):
         split_list = lambda x: x.split()
         data = list(map(split_list, ('').join(y[0]).split('\n')))
-         = []
-        monkeys[i].append(data[0])
+        name = " ".join(data[0][0], data[0][1][0])
+        items = 
+
+        monkeys[i].append(monkey(name, items, test, cond1, cond2))
 
     return monkeys
 
 class monkey():
-    def __init__(self, data):
-        self.name = data[0]
-        self.items = data[1]
-        self.inspect = data[2]
+    def __init__(self, name, items, test, cond1, cond2):
+        self.name = items
+        self.items = items
+        self.test = test
+        self.cond1 = cond1
+        self.cond2 = cond2
 
 
 if __name__ == '__main__': 

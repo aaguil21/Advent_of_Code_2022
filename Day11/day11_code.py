b0f06228699598a395cdef311449247f138b45df
Andres Aguilar <68845693+aaguil21@users.noreply.github.com>
2023-01-04 20:01:11 -0600

    Add day 11

--- Day11/day11_code.py
+++ Day11/day11_code.py
@@ -1,29 +1,36 @@
 import pathlib
 import os
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Monkey():
+    def __init__(self, name, items, test, cond1, cond2):
+        self.name = name
+        self.items = items
+        self.test = test
+        self.cond1 = cond1
+        self.cond2 = cond2
 
 def split_input(filename: str) -> list:
     input = [l for  l in open('input_day11.txt')]
     x = ('\n').join(input).split('\n\n')
     y = ('\n').join(x).split('\n\n')
 
-    monkeys = 
+    monkeys = []
     for i, monkey in enumerate(y):
         split_list = lambda x: x.split()
         data = list(map(split_list, ('').join(y[0]).split('\n')))
-         = []
-        monkeys[i].append(data[0])
+        name = " ".join([data[0][0], data[0][1][0]])
+        items = 
+        test = None
+        cond1 = None
+        cond2 = None
+        monkeys.append(Monkey(name, items, test, cond1, cond2))
 
     return monkeys
 
-class monkey():
-    def __init__(self, data):
-        self.name = data[0]
-        self.items = data[1]
-        self.inspect = data[2]
 
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     file = split_input('input_day11.txt')
     print(file)
